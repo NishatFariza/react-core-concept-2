cf1175432c1c1084b29ea4fb32d22ef48b35c8e9
NishatFariza <nishatfarzia78@gmail.com>
2022-03-18 03:54:43 +0600

    jsx onClick function without call

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  const products =[
+  
+  return (
+    <div className="App">
+      <Counter></Counter>
+    </div>
+  );
+}
+ function Counter (){
+   const [count, setCount] = useState(70);
+   const increaseCount = () => {
+       const newCount = count + 1;
+       setCount(newCount)
+   }
+   return (
+     <div>
+       <h1>Count: {count}</h1>
+       <button onClick={increaseCount}>Increase</button>
+     </div>
+   )
+ }
+/* array of object
+ const products =[
     {name:'Laptop', price : 120000},
     {name:'Phone', price : 20000},
     {name:'Watch', price : 7000},
     {name:'Tablet', price : 700000}
   ]
+*/
+
+  /* pass dynamic similar in look different in data */
+  /*
   return (
-    <div className="App">
-      {
-        products.map(product => <Product name={product.name} price={product.price}></Product>)
-      }
-      {/* <Product name='laptop' price='80000'></Product>
-      <Product name='phone' price='28000'></Product>
-      <Product name='watch' price='8000'></Product> */}
-    </div>
-  );
-}
+  //   <div className="App">
+  //     {
+  //       products.map(product => <Product name={product.name} price={product.price}></Product>)
+  //     }
+  //     <Product name='laptop' price='80000'></Product>
+  //     <Product name='phone' price='28000'></Product>
+  //     <Product name='watch' price='8000'></Product>
+  //   </div>
+  // );*/
+
 
 
 function Product (props) {
