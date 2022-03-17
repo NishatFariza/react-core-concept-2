28c71987d9e02170752657ea50437eec22b5a75d
NishatFariza <nishatfarzia78@gmail.com>
2022-03-18 03:01:28 +0600

    daynamic data

--- src/App.js
+++ src/App.js
@@ -4,8 +4,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <Product name='laptop' price='80000'></Product>
     </div>
   );
+}
+
+
+function Product (props) {
+    return(
+      <div className='product'>
+        <h3>Name: {props.name}</h3>
+        <p>Price: {props.price}</p>
+      </div>
+    )
 }
 
 export default App;
